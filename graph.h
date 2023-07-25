4ba9e71d71f4d54dedadde09fb38b1330a57eb6c
Juraj Brigljević <juraj.brig00@gmail.com>
2023-07-25 17:02:28 +0200

    N_2 početak

--- graph.h
+++ graph.h
@@ -1,23 +1,101 @@
 #pragma once
 
-//#include<bits/stdc++.h>
-#include <iostream>
+#include<bits/stdc++.h>
+
+class RemoveVertices{
+private:
+    std::set<int> remove;
+
+public:
+    RemoveVertices(std::set<int> remove) : remove(remove){}
+
+    bool operator()(int vertex){
+        return remove.erase(vertex);
+    }
+};
 
 class Graph{
 private:
     int no_nodes;
-    bool** neigh_matrix;
-    int* weights;
+    std::vector<std::vector<bool>> neigh_matrix;
+    std::vector<int> weights;
 
 public:
-    void initialize(int n){
-        no_nodes = n;
-        neigh_matrix = new bool*[no_nodes];
-        weights = new int[no_nodes];
+    class Solution{
+    public:
+        Graph& graph;
+        std::vector<int> S;
+        std::vector<std::pair<int, std::list<int>>> S_c;
+        std::vector<int> mi;
+        std::forward_list<int> candidates;
+        std::vector<std::forward_list<std::pair<int, std::list<int>::iterator>>>
+            adj_matrix;
 
-        for(int i = 0; i < no_nodes; ++i){
-            neigh_matrix[i] = new bool[no_nodes]();
-            weights[i] = i % 200 + 1;
+        Solution(Graph& graph) : graph(graph){
+            mi = graph.get_weights();
+            for(int i = 0; i < graph.get_no_nodes(); ++i){
+                S_c.push_back(std::pair<int, std::list<int>>
+                                (i, std::list<int>()));
+            }
+
+            for(int i = 0; i < graph.get_no_nodes(); ++i){
+                for(int j = graph.get_no_nodes(); j >= 0; --j){
+                    if(graph.neigh_matrix[i][j])
+                        adj_matrix[i].push_front(
+                            std::make_pair(j, S_c[j].second.begin()));
+                }
+            }
+        }
+
+        Solution operator=(const Solution& sol){
+            return(Solution(sol));
+        }
+
+        void change(int vertex, bool increase);
+        void insert(std::set<int> vertices);
+        bool solution_contains(int vertex);
+        std::list<int> find_solution_neighbors(int vertex);
+        void insertc(std::set<int> vertices);
+    };
+
+    Graph(int no_nodes, std::vector<std::vector<bool>> neigh_matrix) :
+            no_nodes(no_nodes), neigh_matrix(neigh_matrix){}
+
+    Graph(std::string file_name){
+        std::ifstream file(file_name);
+        std::string tmp;
+        std::string line;
+
+        if(file.is_open()){
+            std::getline(file, line);
+            std::stringstream ss(line);
+
+            ss >> tmp;
+            ss >> tmp;
+            ss >> tmp;
+
+            no_nodes = std::stoi(tmp);
+            std::vector<bool> tmp(no_nodes, false);
+            for(int i = 0; i < no_nodes; ++i) neigh_matrix.push_back(tmp);
+
+            for(int i = 0; i < no_nodes; ++i){
+                weights.push_back(i % 200 + 1);
+            }
+        }
+
+        int fst, snd;
+        while(file.good()){
+            std::getline(file, line);
+            std::stringstream ss(line);
+
+            ss >> tmp;
+            ss >> tmp;
+            fst = std::stoi(tmp);
+            ss >> tmp;
+            snd = std::stoi(tmp);
+
+            neigh_matrix[fst - 1][snd - 1] = true;
+            neigh_matrix[snd - 1][fst - 1] = true;
         }
     }
 
@@ -25,12 +103,19 @@ public:
         return no_nodes;
     }
 
-    void place_edge(int fst, int snd){
-        neigh_matrix[fst][snd] = true;
-        neigh_matrix[snd][fst] = true;
+    int get_weight(int index){
+        return weights[index];
     }
 
-    void print(){
+    std::vector<int> get_weights(){
+        return weights;
+    }
+
+    bool exists_edge(int v1, int v2){
+        return neigh_matrix[v1][v2];
+    }
+
+    void print_graph(){
         for(int i = 0; i < no_nodes; ++i){
             for(int j = i; j < no_nodes; ++j){
                 if(neigh_matrix[i][j]) std::cout << i << " " << j << std::endl;
@@ -38,8 +123,17 @@ public:
         }
     }
 
-    ~Graph(){
-        for(int i = 0; i < no_nodes; ++i) delete[] neigh_matrix[i];
-        delete[] neigh_matrix;
-    }
+    void maximize(Solution& sol);
+    int weight(Solution& sol);
+    Solution perturb(int c, Solution sol);
+    Solution first_improvement(int k, Solution& sol);
+    void local_search(Solution* sol);
+    void accept(Solution* sol, Solution* sol_new, Solution* sol_best, int* i,
+                int* local_best_w, int c2, int c3, int c4);
+    Solution algorithm(int max_iter = 100, int c1 = 1, int c2 = 3, int c3 = 4,
+                        int c4 = 2);
 };
+
+
+// NOTE TO FUTURE SELF: IT MIGHT MAKE SENSE TO USE A LIST IN RemoveVertices,
+// SO THAT ERASE HAS CONSTANT COMPLEXITY
--- main.cpp
+++ main.cpp
@@ -1,15 +1,29 @@
 #include <iostream>
-#include "test.h"
 #include "graph.h"
 
 int main() {
+    /*
     std::string file_name("../test/BHOSLIB/frb30-15-1.txt");
-    Graph graph;
-    Test(file_name, &graph);
+    Graph graph(file_name);
+    graph.print();*/
+    int no_nodes = 8;
+    std::vector<std::vector<bool>> neigh_matrix;
+    std::vector<bool> tmp(no_nodes, false);
+    for(int i = 0; i < no_nodes; ++i) neigh_matrix.push_back(tmp);
 
+    neigh_matrix[0][4] = neigh_matrix[1][5] = neigh_matrix[2][6] =
+    neigh_matrix[3][7] = neigh_matrix[4][5] = neigh_matrix[5][6] = 
+    neigh_matrix[6][7] = neigh_matrix[7][4] = true;
+    for(int i = 0; i < no_nodes; ++i){
+        for(int j = 0; j < no_nodes; ++j){
+            if(neigh_matrix[i][j]) neigh_matrix[j][i] = true;
+        }
+    }
+    
+    Graph graph(no_nodes, neigh_matrix);
+    //graph.print();
+    //algorithm(&graph);
 }
-
-// WEIGHTS ARE CALCULATED AS: (i mod 200) + 1, where i is the index of a vertex
 
 /*NOTE TO FUTURE SELF: DIMACS IS USED FOR THE CLIQUE PROBLEM, SO WE CONSIDER
 ITS COMPLEMENTARY GRAPHS, THERE IS A COMMENT ABOUT BHOSLIB ON PAGE 6 THAT I DO
